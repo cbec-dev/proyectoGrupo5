057b283bf12a63ffc386a05c4d5a6299a0ae0c20
Isacc <isaac.espinoza@usach.cl>
2018-05-08 10:59:03 -0300

    modificacion subir enunciado

--- frontend/src/Enunciado.js
+++ frontend/src/Enunciado.js
@@ -10,49 +10,78 @@ import {FormGroup, ControlLabel, FormControl, HelpBlock, Navbar, NavItem, MenuIt
 
 
 class Enunciado extends Component {
-    constructor(props, context) {
-      super(props, context);
-  
-      this.handleChange = this.handleChange.bind(this);
-  
-      this.state = {
-        value: ''
-      };
+    constructor(props) {
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.state = {
+            isLoading: false,
+            nameStatement:"",
+            text:"",
+
+        };
     }
-  
-    getValidationState() {
-      const length = this.state.value.length;
-      if (length > 10) return 'success';
-      else if (length > 5) return 'warning';
-      else if (length > 0) return 'error';
-      return null;
+    subirFormulario(e) {
+        console.log("formulario enviado c:");
+        this.statement = {nameStatement: "", text: ""}
+        this.statement.nameStatement = e.nameStatement;
+        this.statement.text = e.text; 
+        if(this.statement.nameStatement==="" || this.statement.text ===""){
+            console.log("Debe llenar todos las casillas");
+            return;
+        }
+        else{
+            this.limpiarValores(1);
+            console.log("Usuario: "+ this.statement);
+            console.log("Datos: "+ this.statement.nameStatement);
+            console.log("Datos: "+ this.statement.text);
+            
+            fetch('http://104.236.68.75:8080/backendGrupo5/api/add?codigo='+this.user.codigo+'&nombre='+this.user.nombre+'&fecha='+this.user.fecha+'&categoria='+this.user.categoria+'&precio='+this.user.precio)
+            .then(response => console.log("Producto Agregado"+response)) 
+            }
+       
+        return;
+        }
+    limpiarValores(i){
+        if(i===1){
+            this.setState({isLoading: false, nameStatement:"", text:""});
+            this.render();
+        }
     }
-  
-    handleChange(e) {
-      this.setState({ value: e.target.value });
-    }
-  
-    render() {
-      return (
-        <form>
-          <FormGroup
-            controlId="formBasicText"
-            validationState={this.getValidationState()}
-          >
-            <ControlLabel>Working example with validation</ControlLabel>
-            <FormControl 
-              type="text"
-              name="formulario"
-              value={this.state.value}
-              placeholder="Enter text"
-              onChange={this.handleChange}
-            />
-            <FormControl.Feedback />
-            <HelpBlock>Validation is based on string length.</HelpBlock>
-          </FormGroup>
-        </form>
-      );
-    }
-  }
-  
-export default Enunciado;
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name; 
+        this.setState({
+            [name]: value
+            });
+        console.log(name, value, target);
+        }
+
+
+        render() {
+                return (
+                    <form>
+                    <div>
+                        <label> Nombre Enunciado:  </label>
+                        <input name= "nameStatement" type = "text" value={this.state.nameStatement}
+                        onChange = {this.handleInputChange} />
+                    </div>
+                    
+                    <div>
+                        <label> Texto:  </label>
+                        <textarea name= "text" type = "text" value={this.state.text} 
+                        onChange = {this.handleInputChange} />
+                    </div>
+                    <div>
+                      <button type="button" onClick={(e) => this.subirFormulario(this.state)}>Subir Enunciado</button>
+                      <button type="button" onClick={(e) => this.limpiarValores(1)}>Limpiar Casillas</button>
+                    </div>
+                  </form>
+    
+                    
+                );
+            }
+          }
+    
+    
+    export default Enunciado;
