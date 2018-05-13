cb6aea0b94f23ed57d5b2448d3d2c475981e9e54
Powei <cristobal.becerraf@usach.cl>
2018-05-13 17:50:13 -0400

    Se agregan tests de setters para User

--- backend/src/test/java/com/mingeso/grupo5/proyecto/entities/UserTests.java
+++ backend/src/test/java/com/mingeso/grupo5/proyecto/entities/UserTests.java
@@ -30,6 +30,7 @@ public class UserTests {
         Assert.assertNotNull(testUser.getIdUser());
     }
 
+    @Test
     public void getCorreo() {
         String correo = "user@email.com";
         String username = "user";
@@ -45,6 +46,7 @@ public class UserTests {
         Assert.assertNotNull(testUser.getCorreo());
     }
 
+    @Test
     public void getUserName() {
         String correo = "user@email.com";
         String username = "user";
@@ -60,6 +62,7 @@ public class UserTests {
         Assert.assertNotNull(testUser.getUserName());
     }
 
+    @Test
     public void getSection() {
         String correo = "user@email.com";
         String username = "user";
@@ -75,6 +78,7 @@ public class UserTests {
         Assert.assertNotNull(testUser.getSection());
     }
 
+    @Test
     public void getCareer() {
         String correo = "user@email.com";
         String username = "user";
@@ -88,6 +92,57 @@ public class UserTests {
         testUser.setSection(section);
 
         Assert.assertNotNull(testUser.getCareer());
+    }
+
+
+    @Test
+    public void setIdUser() {
+        User testUser = new User();
+
+        testUser.setIdUser(2);
+        Assert.assertEquals(Integer.valueOf(2), testUser.getIdUser());
+
+    }
+
+    @Test
+    public void setCorreo() {
+        String correo = "user@email.com";
+
+        User testUser = new User();
+        testUser.setCorreo(correo);
+
+        Assert.assertEquals(correo, testUser.getCorreo());
+    }
+
+    @Test
+    public void setUserName() {
+        String username = "user";
+
+        User testUser = new User();
+        testUser.setUserName(username);
+
+        Assert.assertEquals(username, testUser.getUserName());
+    }
+
+    @Test
+    public void setSection() {
+        Section section = new Section();
+
+        User testUser = new User();
+        testUser.setSection(section);
+
+        Assert.assertEquals(section, testUser.getSection());
+
+    }
+
+    @Test
+    public void setCareer() {
+        Career career = new Career();
+
+        User testUser = new User();
+        testUser.setCareer(career);
+
+        Assert.assertEquals(career, testUser.getCareer());
     }
 
 
