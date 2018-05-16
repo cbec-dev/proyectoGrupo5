b1aa94ef0dd6a02aceb7770bd36846536361dcff
Luis Abello <luis.abello@usach.cl>
2018-05-16 02:02:28 -0400

    Se crean test para SectionController y SolutionController

---
+++ backend/src/test/java/com/mingeso/grupo5/proyecto/controllers/SolutionControllerTest.java
@@ -0,0 +1,43 @@
+package com.mingeso.grupo5.proyecto.controllers;
+import org.junit.Test;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import com.mingeso.grupo5.proyecto.controllers.UserControllerTests;
+import com.mingeso.grupo5.proyecto.entities.Solution;
+import com.mingeso.grupo5.proyecto.repositories.SolutionRepository;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolutionControllerTest {
+	@Autowired 
+	private SolutionRepository solutionRepository;
+	@Test
+    public void getAllSolution() {
+		
+    }
+	@Test
+    public void addNewSolution() {
+		
+    }
+	@Test
+    public void updateSolution() {
+		
+    }
+	@Test
+    public void findOneSolution() {
+		
+    }
+	@Test
+    public void deletesolution() {
+		
+    }
+	
+	
+}
