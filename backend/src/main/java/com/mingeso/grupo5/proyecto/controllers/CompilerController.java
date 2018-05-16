13eea08fd7924715788b4f4ef690f42b2402349e
Powei <cristobal.becerraf@usach.cl>
2018-05-16 12:58:32 -0400

    Conexión con glot.io desde backend funciona correctamente

---
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/controllers/CompilerController.java
@@ -0,0 +1,40 @@
+package com.mingeso.grupo5.proyecto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+import com.mingeso.grupo5.proyecto.helpers.Compiler;
+
+@Controller
+@CrossOrigin(origins = "http://104.236.68.75:8080/frontendGrupo5")
+@RequestMapping(path="/api/compiler")
+
+public class CompilerController {
+    @Autowired 
+    @GetMapping(path="/languages")
+	public @ResponseBody String getAll() throws IOException {
+
+        Compiler compiler = new Compiler();
+        
+        String languages = compiler.getLanguages();
+		return languages;
+    }
+
+    @GetMapping(path="/runCode")
+    public @ResponseBody String runCode() throws IOException {
+
+        Compiler compiler = new Compiler();
+        
+        String out = compiler.run("print(400)");
+		return out;
+    }
+    
+}
