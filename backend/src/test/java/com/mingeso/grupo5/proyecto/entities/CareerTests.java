4d08622f643681722d93622cb78bbbee9834c88b
Marcela Rivera <marcela.rivera.c@usach.cl>
2018-05-17 16:33:31 -0400

    Test jUnit para entitie Career

---
+++ backend/src/test/java/com/mingeso/grupo5/proyecto/entities/CareerTests.java
@@ -0,0 +1,21 @@
+package com.mingeso.grupo5.proyecto.entities;
+
+import com.mingeso.grupo5.proyecto.entities.Career;
+import com.mingeso.grupo5.proyecto.entities.Section;
+import com.mingeso.grupo5.proyecto.entities.User;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CareerTests {
+
+    
+
+}
