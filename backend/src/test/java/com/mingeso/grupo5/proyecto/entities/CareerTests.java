63cd734c789946d08e1b027b735e2ab69fb9f9c7
Marcela Rivera <marcela.rivera.c@usach.cl>
2018-05-17 16:41:30 -0400

    Se implementa jUnit para metodos get de entitie Career

--- backend/src/test/java/com/mingeso/grupo5/proyecto/entities/CareerTests.java
+++ backend/src/test/java/com/mingeso/grupo5/proyecto/entities/CareerTests.java
@@ -16,6 +16,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CareerTests {
 
+    @Test
+    public void getIdCareer() {
+        Integer idCareer = 1;
+        String careerName = "user";
+        
+
+        Career testCareer = new Career();
+        testCareer.setIdCareer(idCareer);
+        testCareer.setCareerName(careerName);
+        
+
+        Assert.assertNotNull(testCareer.getIdCareer());
+    }
+
+    @Test
+    public void getCareerName() {
+        Integer idCareer = 1;
+        String careerName = "user";
+        
+
+        Career testCareer = new Career();
+        testCareer.setIdCareer(idCareer);
+        testCareer.setCareerName(careerName);
+        
+
+        Assert.assertNotNull(testCareer.getCareerName());
+    }
+
+    
     
 
 }
