818ffbc65484b77de9aa86aafbb5d46c3ba0d2f9
Marcela Rivera <marcela.rivera.c@usach.cl>
2018-05-17 11:16:11 -0400

    Prueba jUnit para Career, falta implementar otras funciones

---
+++ backend/src/test/java/com/mingeso/grupo5/proyecto/controllers/CareerControllerTests.java
@@ -0,0 +1,28 @@
+
+package com.mingeso.grupo5.proyecto.controllers;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.sql.Date;
+
+import com.mingeso.grupo5.proyecto.entities.Career;
+import com.mingeso.grupo5.proyecto.repositories.CareerRepository;
+import com.mingeso.grupo5.proyecto.controllers.CareerController;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CareerControllerTests {
+
+    @Test
+    public void updateCareer() {
+    }
+}
+
