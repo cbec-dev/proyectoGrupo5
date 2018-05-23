64a86f3afb079d954d01d7f01e03322e028c2766
Isacc <isaac.espinoza@usach.cl>
2018-05-23 18:39:35 -0400

    import.sql

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,34 @@
+
+insert into user (id_user, correo, user_name, user_type) VALUES ("1", "isaac.espinoza@usach.cl", "isaac espinoza","2");
+
+insert into user (id_user, correo, user_name, user_type) VALUES ("2", "algo@usach.cl", "profesor uwu","3");
+insert into user (id_user, correo, user_name, user_type) VALUES ("3", "algo2@usach.cl", "profesor2","3");
+insert into user (id_user, correo, user_name, user_type) VALUES ("4", "algo3@usach.cl", "profesor3","3");
+insert into user (id_user, correo, user_name, user_type) VALUES ("5", "algo4@usach.cl", "profesor4","3");
+insert into user (id_user, correo, user_name, user_type) VALUES ("6", "algo5@usach.cl", "profesor5","3");
+insert into user (id_user, correo, user_name, user_type) VALUES ("7", "algo6@usach.cl", "profesor6","3");
+
+insert into section (id_section, section_name, profesor_id_user) VALUES ("1", "A-1", "2");
+insert into section (id_section, section_name, profesor_id_user) VALUES ("2", "B-2", "3");
+insert into section (id_section, section_name, profesor_id_user) VALUES ("3", "C-3", "4");
+insert into section (id_section, section_name, profesor_id_user) VALUES ("4", "D-4", "5");
+insert into section (id_section, section_name, profesor_id_user) VALUES ("5", "E-5", "6");
+insert into section (id_section, section_name, profesor_id_user) VALUES ("6", "F-6", "7");
+
+insert into career (id_career, career_name) VALUES ("1", "Ing. Civil Informatica");
+insert into career (id_career, career_name) VALUES ("2", "Ing. Civil Industrial");
+insert into career (id_career, career_name) VALUES ("3", "Ing. Civil Electrica");
+insert into career (id_career, career_name) VALUES ("4", "Ing. Civil Mecanica");
+insert into career (id_career, career_name) VALUES ("5", "Ing. Ejec. Informatica");
+insert into career (id_career, career_name) VALUES ("6", "Ing. Civil Metalurgica");
+insert into career (id_career, career_name) VALUES ("7", "Ing. Civil");
+
+
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("8", "alumno@usach.cl", "alumno1","1", "1", "1");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("9", "alumno@usach.cl", "alumno2","1", "2", "2");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("10", "alumno@usach.cl", "alumno3","1", "3", "1");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("11", "alumno@usach.cl", "alumno4","1", "4", "2");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("12", "alumno@usach.cl", "alumno5","1", "2", "3");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("13", "alumno@usach.cl", "alumno6","1", "1", "4");
+insert into user (id_user, correo, user_name, user_type, career_id_career, section_id_section) VALUES ("14", "alumno@usach.cl", "alumno7","1", "4", "4");
+
