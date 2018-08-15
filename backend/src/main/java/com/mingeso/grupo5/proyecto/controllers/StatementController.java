b09cffdbe598d045444032e8d63a00a02b00a5e6
Isacc <isaac.espinoza@usach.cl>
2018-08-15 16:43:27 -0300

    cambio fechas

--- backend/src/main/java/com/mingeso/grupo5/proyecto/controllers/StatementController.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/controllers/StatementController.java
@@ -63,8 +63,8 @@ public class StatementController {
 			@RequestParam String statementText,
 			@RequestParam Integer section,
 			@RequestParam String header,
-			@RequestParam Date finalDate,
-			@RequestParam Date initialDate,
+			@RequestParam String finalDate,
+			@RequestParam String initialDate,
 			@RequestParam(value = "expectedSolution", required = false) List<String> expectedSolution,
 			@RequestParam(value = "testCases", required = false) List<String> testCases) 
             {
@@ -74,18 +74,16 @@ public class StatementController {
 		Statement n = new Statement();
 		List<ExpectedSolution> expectSol=new ArrayList<ExpectedSolution>();
 		List<TestCase> testC = new ArrayList<TestCase>();
-		//Date date = new Date();
-		//Date initial = new Date();
-		//LocalDate localDate = LocalDate();
-		//Date initialDate = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		//DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		//try {
-		//	 date = formatter.parse(finalDate);
-		//	 initial = formatter.parse(initialDate);
-		//	}
-		//   catch (Exception e) {
+		Date date = new Date();
+		Date initial = new Date();
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			 date = formatter.parse(finalDate);
+			 initial = formatter.parse(initialDate);
+			}
+		   catch (Exception e) {
 			
-		//   }
+		   }
 		for(String str : expectedSolution){
     		ExpectedSolution temp = new ExpectedSolution();
     		temp.setExpectedSolution(str);
@@ -100,15 +98,19 @@ public class StatementController {
 			}
 		//System.out.println("Fecha sin formato: " + finalDate);
 		//System.out.println("Fecha formateade: " + date);
-		Date javaDateFinal=new SimpleDateFormat("yy-MM-dd HH:mm:ss").parse(finalDate);
-		Date javaDateInitial=new SimpleDateFormat("yy-MM-dd HH:mm:ss").parse(initialDate);
+		/*try{
+			Date javaDateFinal=new SimpleDateFormat("yy-MM-dd HH:mm:ss").parse(finalDate);
+			Date javaDateInitial=new SimpleDateFormat("yy-MM-dd HH:mm:ss").parse(initialDate);
+		}
+			catch (Exception e){
 
+			}*/
 		n.setStatementName(statementName);
         n.setStatementText(statementText);
 		n.setSection(s);
 		n.setHeader(header);
-		n.setFinalDate(javaDateFinal);
-		n.setInitialDate(javaDateInitial);
+		n.setFinalDate(date);
+		n.setInitialDate(initial);
 		//expectSol.setExpectedSolution(expectedSolution);
 		//expectedSolutionRepository.save(expectSol);
 		n.setExpectedSolution(expectSol);
