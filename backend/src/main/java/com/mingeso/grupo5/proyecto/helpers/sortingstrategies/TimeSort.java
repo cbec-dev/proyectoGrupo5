da6f6048046310444ab5fedda8f50e38dd682f02
Powei <cristobal.becerraf@usach.cl>
2018-08-18 22:08:57 -0300

    Se borra metodo innecesario

--- backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/sortingstrategies/TimeSort.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/sortingstrategies/TimeSort.java
@@ -6,9 +6,6 @@ import com.mingeso.grupo5.proyecto.entities.Solution;
 import com.mingeso.grupo5.proyecto.helpers.SolutionSorterStrategy;
 
 public class TimeSort implements SolutionSorterStrategy {
-    public void compressFiles(ArrayList<Solution> solutios) {
-      
-    }
 
 	@Override
 	public void sortSolutions(ArrayList<Solution> solutions) {
