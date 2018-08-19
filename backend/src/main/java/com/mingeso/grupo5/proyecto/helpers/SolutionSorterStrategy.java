7045b574c73acfbc050e44076d1d51a282f58bf8
Powei <cristobal.becerraf@usach.cl>
2018-08-18 22:00:33 -0300

    Se imlpementa interfaz strategy completamente

--- backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterStrategy.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterStrategy.java
@@ -1,4 +1,9 @@
 package com.mingeso.grupo5.proyecto.helpers;
 
+import java.util.ArrayList;
+
+import com.mingeso.grupo5.proyecto.entities.Solution;
+
 public interface SolutionSorterStrategy {
+    public void sortSolutions(ArrayList<Solution> solutions);
  }
