e60011054b1ffcef6a93511a359c9368cd181542
Powei <cristobal.becerraf@usach.cl>
2018-08-18 22:06:53 -0300

    Se implementa template para sortear por tiempo

---
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/sortingstrategies/TimeSort.java
@@ -0,0 +1,12 @@
+package com.mingeso.grupo5.proyecto.helpers.sortingstrategies;
+
+import java.util.ArrayList;
+
+import com.mingeso.grupo5.proyecto.entities.Solution;
+import com.mingeso.grupo5.proyecto.helpers.SolutionSorterStrategy;
+
+public class TimeSort implements SolutionSorterStrategy {
+    public void compressFiles(ArrayList<Solution> solutios) {
+      
+    }
+  }
