116e3cffa10fd8a764ac88195c237c79a2262f48
Powei <cristobal.becerraf@usach.cl>
2018-08-18 22:57:31 -0300

    Se implementa setter de la estrategia a usar

--- backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterContext.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterContext.java
@@ -2,5 +2,9 @@ package com.mingeso.grupo5.proyecto.helpers;
 
 
 public class SolutionSorterContext {
+    private SolutionSorterStrategy strategy;
 
+    public void setSolutionSorterStrategy(SolutionSorterStrategy strategy) {
+      this.strategy = strategy;
+    }
   }
