48f3d30216c176a39953961b96d7711ba89567f4
Powei <cristobal.becerraf@usach.cl>
2018-08-18 23:03:38 -0300

    Se termina de implementar el context

--- backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterContext.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/SolutionSorterContext.java
@@ -1,10 +1,20 @@
 package com.mingeso.grupo5.proyecto.helpers;
 
+import java.util.ArrayList;
+
+import com.mingeso.grupo5.proyecto.entities.Solution;
 
 public class SolutionSorterContext {
     private SolutionSorterStrategy strategy;
 
+    //Este método setea la estrategia a utilizar
     public void setSolutionSorterStrategy(SolutionSorterStrategy strategy) {
       this.strategy = strategy;
     }
-  }
+
+
+    //Este método utiliza el algoritmo de la estrategia seteada
+    public void createArchive(ArrayList<Solution> solutions) {
+    strategy.sortSolutions(solutions);
+    }
+}
