f784a1848bddbd189149e70ef44506818a26c9c7
Powei <cristobal.becerraf@usach.cl>
2018-08-18 22:07:59 -0300

    Se crea método en Timesort

--- backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/sortingstrategies/TimeSort.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/sortingstrategies/TimeSort.java
@@ -9,4 +9,9 @@ public class TimeSort implements SolutionSorterStrategy {
     public void compressFiles(ArrayList<Solution> solutios) {
       
     }
+
+	@Override
+	public void sortSolutions(ArrayList<Solution> solutions) {
+		
+	}
   }
