1aa35d5f52c00bc2a7bcb281da0162219529f4cb
Isacc <isaac.espinoza@usach.cl>
2018-08-21 01:00:03 -0300

    fix ruta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -120,7 +120,7 @@ class App extends Component {
         <Route path="/ListarEnunciados" component={()=><ListarEnunciados typeUser={this.state.user.userType} history={customHistory} activeUser={this.state.user} callBackFromParentStatement= {this.myCallBackStatement}/>}/>
         <Route path="/ListarSoluciones" component={()=><ListarSoluciones typeUser={this.state.user.userType} history={customHistory} activeUser={this.state.user}/>}/>
         <Route path="/VerSolucion" component={()=><verSolucion typeUser={this.state.user.userType} history={customHistory} callBackFromParentStatement= {this.myCallBackStatement} activeUser={this.state.user}/>}/>
-        <Route path="/MostrarSolucion" component={()=><MostrarSolucion typeUser={this.state.user.userType} history={customHistory} activeUser={this.state.user}/>}/>
+        <Route path="/MostrarSolucion" component={MostrarSolucion}/>
 
 
       </Switch>
