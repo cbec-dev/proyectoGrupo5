3126ec416a03d833ee0e0ecd74278142f19b3e6f
Powei <cristobal.becerraf@usach.cl>
2018-08-25 19:33:26 -0300

    fix en getStats

--- backend/src/main/java/com/mingeso/grupo5/proyecto/controllers/CompilerController.java
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/controllers/CompilerController.java
@@ -150,31 +150,32 @@ public class CompilerController {
             @RequestParam String filter,
             @RequestParam String method) throws IOException {
 
-                //Se obtiene la lista de soluciones base
-                ArrayList<Solution> solutions = (ArrayList<Solution>) solutionRepository.findAll();
+                // //Se obtiene la lista de soluciones base
+                // ArrayList<Solution> solutions = (ArrayList<Solution>) solutionRepository.findAll();
     
-                //Se crea contexto y se elige método a utilizar
-                SolutionStatsContext ctx = new SolutionStatsContext();
-                switch (method) {
-                    case "time":	ctx.setStatsStrategy(new TimeStats());
-                                    break;
+                // //Se crea contexto y se elige método a utilizar
+                // SolutionStatsContext ctx = new SolutionStatsContext();
+                // switch (method) {
+                //     case "time":	ctx.setStatsStrategy(new TimeStats());
+                //                     break;
                     
-                    default:        ctx=null;
-                                    break;
-                }
-    
-                // switch (filter) {
-                //     case "career":  
-                //                     break;
-                //     case "section": //solutions = findBySection(id);
-                //                     break;
-                //     default:        solutions = null;
+                //     default:        ctx=null;
                 //                     break;
                 // }
     
-                if (solutions.size()==0) return "ERROR: no se han encontrado soluciones.";
+                // // switch (filter) {
+                // //     case "career":  
+                // //                     break;
+                // //     case "section": //solutions = findBySection(id);
+                // //                     break;
+                // //     default:        solutions = null;
+                // //                     break;
+                // // }
     
-                String out = ctx.getStats(solutions);
+                // if (solutions.size()==0) return "ERROR: no se han encontrado soluciones.";
+    
+                // //String out = ctx.getStats(solutions);
+                String out = method + " - " + filter;
     
                 return out;
         }
