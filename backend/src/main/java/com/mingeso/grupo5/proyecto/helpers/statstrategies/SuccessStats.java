815e909417cd8e8a026c34e544e04c1b4581ef5e
Powei <cristobal.becerraf@usach.cl>
2018-08-26 13:00:14 -0300

    se agrega strategy de exito de soluciones

---
+++ backend/src/main/java/com/mingeso/grupo5/proyecto/helpers/statstrategies/SuccessStats.java
@@ -0,0 +1,29 @@
+package com.mingeso.grupo5.proyecto.helpers.statstrategies;
+
+import java.util.ArrayList;
+
+import com.mingeso.grupo5.proyecto.entities.Solution;
+import com.mingeso.grupo5.proyecto.helpers.SolutionStatsStrategy;
+
+public class SuccessStats implements SolutionStatsStrategy {
+
+	@Override
+	public Float getStats(ArrayList<Solution> solutions) {
+		double totalSuccess = 0;	//Exito total
+		float promSuccess = 0;	//Exito promedio
+		int size = solutions.size(); //Cantidad total de soluciones
+		for(Solution sol : solutions){
+			if(sol.getTime()!=null) {
+				totalSuccess += sol.getpSuccess();
+			}
+		}
+		if (totalSuccess==0) {
+			promSuccess = (float) 0;
+		}
+		else {
+			promSuccess = (float) totalSuccess/size;
+		}
+
+		return promSuccess;
+	}
+}
